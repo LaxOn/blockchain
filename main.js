352a56a9b10bbed0abc32d25dc26fa0244952066
Bryan Lacson <br_lacson@yahoo.com>
2019-08-08 14:46:44 -0400

    added more simple functions and testing console log

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-	constructor(index, timestamp, data, previiousHash = '') {
-		this.index. = index;
+	constructor(index, timestamp, data, previousHash = '') {
+		this.index = index;
 		this.timestamp = timestamp;
-		this.previiousHash = previiousHash;
+		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
 	}
 
 	calculateHash() {
-		return SHA256(this.index  + this.previiousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.index  + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
+
+class Blockchain {
+	constructor() {
+		this.chain = [this.createGenesisBlock()];
+	}
+
+	createGenesisBlock(){
+		return new Block(0, "01/01/2019", "Genesis block", "0");
+	}
+
+	getLatestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock) {
+		newBlock.previiousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock)
+	}
+}
+
+let laxCoin = new Blockchain();
+laxCoin.addBlock(new Block(1, "01/08/2019"), { amount: 4 });
+laxCoin.addBlock(new Block(2, "08/08/2019"), { amount: 10 });
+
+console.log(JSON.stringify(laxCoin, null, 4));
